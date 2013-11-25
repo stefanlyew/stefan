2acb09638abfbb0ad2cb402874fef8d44bb55691
Stefan Lyew <stefan.lyew@gmail.com>
2013-11-24 22:42:16 -0500

    Started a middleman blog and added styling

---
+++ config.rb
@@ -0,0 +1,115 @@
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  blog.prefix = "blog"
+  blog.permalink = "/:title.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
+
+page "/feed.xml", :layout => false
+
+set :markdown_engine, :kramdown
+set :markdown, :layout_engine => :erb, 
+               :tables => true, 
+               :autolink => true,
+               :smartypants => true
+### 
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+page "blog/*", :layout => :article_layout
+# Per-page layout changes:
+# 
+# With no layout
+# page "/path/to/file.html", :layout => false
+# 
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+# 
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+ helpers do
+   def header_helper(image_path, link='/')
+      <<-eos
+      <header class='global-header' style="background-image: url('#{image_path}');">
+        <h1 class='logo'>Rob McLarty</h1>
+        <a alt='About Rob McLarty' class='main-button' href='#{link}' id='main-button' title='About Rob McLarty'>
+        </a>
+      </header>
+      eos
+   end
+ end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+set :fonts_dir, 'fonts'
+# Build-specific configuration
+configure :build do
+  require "favicon_maker"
+  activate :favicon_maker
+  activate :minify_css
+  activate :minify_javascript
+  activate :cache_buster
+  
+  # Use relative URLs
+  # activate :relative_assets
+  
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+  
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
