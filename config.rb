a661c4ca26e585eae07e155b7389af27452f508f
Stefan Lyew <stefan.lyew@gmail.com>
2013-11-26 13:24:32 -0500

    rename article Lincoln, enable relative in config

--- config.rb
+++ config.rb
@@ -5,9 +5,8 @@
 # Time.zone = "UTC"
 
 activate :blog do |blog|
-  blog.prefix = "blog"
-  blog.permalink = "/:title.html"
-  blog.sources = "blog/:year-:month-:day-:title.html"
+  blog.prefix = "blog/"
+  blog.permalink = ":title.html"
   # blog.taglink = "tags/:tag.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
@@ -15,7 +14,7 @@ activate :blog do |blog|
   # blog.year_link = ":year.html"
   # blog.month_link = ":year/:month.html"
   # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".markdown.erb"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
@@ -76,7 +75,7 @@ page "blog/*", :layout => :article_layout
 
 # Methods defined in the helpers block are available in templates
  helpers do
-   def header_helper(image_path, link='/')
+   def header_helper(image_path, link='/index.html')
       <<-eos
       <header class='global-header' style="background-image: url('#{image_path}');">
         <h1 class='logo'>Stefan Lyew</h1>
@@ -94,6 +93,8 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
 set :fonts_dir, 'fonts'
+
+activate :relative_assets
 # Build-specific configuration
 configure :build do
   require "favicon_maker"
@@ -101,15 +102,16 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :cache_buster
-  
+  activate :asset_hash
+  set :relative_links, true
+
   # Use relative URLs
-  # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
